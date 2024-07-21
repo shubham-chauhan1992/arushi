b5114bdb81618adeb50c99a59a34e0b6215e6707
shubham-chauhan1992 <shubham-chauhan1992>
2024-07-21 05:09:36 +0000

    day1- read csv variations

--- sample notebook.py
+++ sample notebook.py
@@ -2,3 +2,35 @@
 # MAGIC %md
 # MAGIC # This cis a sample code for reading a dfile
 # MAGIC
+
+# COMMAND ----------
+
+display(dbutils.fs.ls('/FileStore/tables'))
+
+
+# COMMAND ----------
+
+df=spark.read.format('csv').options(header="true",inferSchema="true").load('dbfs:/FileStore/tables/customers_20240710.csv')
+
+display(df)
+display(df2)
+
+# COMMAND ----------
+
+df2=df.select("customer Id")
+display(df2)
+
+# COMMAND ----------
+
+df.createOrReplaceTempView("customer")
+df.createOrReplaceTempView("customer_copy")
+
+
+
+# COMMAND ----------
+
+df3=spark.sql("select a.*,b.* from customer a inner join customer b on a.Index=b.Index") 
+
+# COMMAND ----------
+
+display(df3)
